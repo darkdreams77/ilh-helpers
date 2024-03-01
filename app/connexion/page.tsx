b88e64acd97ca2af127381ede768713d87cef420
darkdreams77 <x.dark-dreams@hotmail.fr>
2024-03-01 14:48:23 +0100

    fix: Suspense errors

--- app/connexion/page.tsx
+++ app/connexion/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
+import { Suspense } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 
 import { LogIn } from "@/components/custom/log-in"
 import { SignIn } from "@/components/custom/sign-in"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-export default function Connexion() {
+function ConnexionWithParams() {
   const router = useRouter()
   const params = useSearchParams()
   const value = params.get("type") as string
@@ -34,5 +35,13 @@ export default function Connexion() {
         <LogIn />
       </TabsContent>
     </Tabs>
+  )
+}
+
+export default function Connexion() {
+  return (
+    <Suspense>
+      <ConnexionWithParams />
+    </Suspense>
   )
 }
