73e8c7d3d71fd4522ec8c803d8f77f514af769d5
darkdreams77 <darkdreams77@users.noreply.github.com>
2024-03-01 03:28:51 +0100

    Update header.tsx

--- components/custom/header.tsx
+++ components/custom/header.tsx
@@ -12,7 +12,7 @@ export function Header() {
   return (
     <header className={variants}>
       <div className="max-w-8xl mx-auto">
-        <div className="flex items-center justify-between py-4">
+        <div className="flex items-center justify-between p-4 md:py-4">
           <div className="flex items-center justify-start gap-4">
             <Link href="/">
               <Image
