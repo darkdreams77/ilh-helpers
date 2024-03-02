3c84ffb88c6f9d3404d0562b6eed0c8f25ad9fa4
darkdreams77 <x.dark-dreams@hotmail.fr>
2024-03-02 13:15:08 +0100

    fix(app): modules not found

---
+++ components/ui/loader.tsx
@@ -0,0 +1,7 @@
+import { Loader2, type LucideProps } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+
+export const Loader = ({ className, ...props }: LucideProps) => {
+  return <Loader2 {...props} className={cn(className, "animate-spin")} />
+}
